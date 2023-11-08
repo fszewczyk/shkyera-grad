043afd8b15b4cc1d57bbe170cbf8eef8e651c1d7
szewczyk.franciszek02 <szewczyk.franciszek02@gmail.com>
2023-11-08 07:08:01 +0100

    LICENSE info

--- include/src/core/Type.hpp
+++ include/src/core/Type.hpp
@@ -1,3 +1,10 @@
+/**
+ * Copyright © 2023 Franciszek Szewczyk. None of the rights reserved.
+ * This code is released under the Beerware License. If you find this code useful or you appreciate the work, you are
+ * encouraged to buy the author a beer in return.
+ * Contact the author at szewczyk.franciszek02@gmail.com for inquiries and support.
+ */
+
 #pragma once
 
 namespace shkyera::Type {
